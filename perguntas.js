c4af8b0f8d44efb5a92eee4a5ae16448a6c64873
fabicsm <fabiola.silva.martins@escola.pr.gov.br>
2025-06-26 18:46:35 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,41 @@
+criaCartao(
+    'felinos',
+    'Quais os principais felinos',
+    'Leão, Leopardo, Linces, Pumas e Gatos'
+)
+
+criaCartao(
+    'Historia',
+    'Em que ano se iniciou a grande guerra?',
+    'Em 1914'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual o país mais populoso do mundo',
+    'É a índia, com estimadamente 1,428 Bilhão de pessoas.'
+)
+
+criaCartao(
+    'biologia',
+    'O que são mutações',
+    'São alterações no material genético (DNA ou RNA) de um organismo.'
+)
+
+criaCartao(
+    'Ed financeira',
+    'O que são investimentos de renda fixa?',
+    'São conhecidos por ser seguros e também são aqueles onde a forma de retorno é o juros.'
+)
+
+criaCartao(
+    'Astronomia',
+    'qual o maior planeta do universo?',
+    'O maior planeta é o ROXs 42Bb, ele possui uma massa 318 vezes maior que a da terra e é gasoso.'
+)
+
+criaCartao(
+    'Fisica',
+    'qual a primeira leí de newton?',
+    'A primeira leí de Newton é a da Inercia, na qual um corpo em repouso permanece em repouso a menos que uma força externa atua sobre ele.'
+)
